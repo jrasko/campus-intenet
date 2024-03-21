76f551053d106c01b923079be7ebf0d0305ccfbf
Jannik Raskob <jannik.raskob@snabble.io>
2024-03-21 13:16:02 +0100

    do not map manufacturer on inputConfig

--- frontend/utils/utils.ts
+++ frontend/utils/utils.ts
@@ -1,12 +1,11 @@
-
 export function formatMac(str: string): string {
     str = str.toUpperCase()
     str = str.replace(/[^0-9A-F]/g, '')
 
     let out = ""
     for (let i = 1; i <= Math.min(str.length, 12); i++) {
-        out += str[i-1]
-        if (i%2 == 0 && i < str.length){
+        out += str[i - 1]
+        if (i % 2 == 0 && i < str.length) {
             out += ":"
         }
     }
@@ -28,7 +27,12 @@ export function toInputMember(i: MemberConfig): InputMember {
     return {
         id: i.id,
         comment: i.comment,
-        dhcpConfig: i.dhcpConfig,
+        dhcpConfig: {
+            id: i.dhcpConfig.id,
+            ip: i.dhcpConfig.ip,
+            mac: i.dhcpConfig.mac,
+            disabled: i.dhcpConfig.disabled,
+        },
         email: i.email,
         firstname: i.firstname,
         hasPaid: i.hasPaid,
