2189cf287c4d4f97538afe3ea57ccd04e4589b4f
Jannik Raskob <jannik.raskob@snabble.io>
2024-05-08 20:45:11 +0200

    add a footer

--- frontend/app.vue
+++ frontend/app.vue
@@ -6,33 +6,37 @@
         <NuxtPage @login="setLogin" @logout="logout"/>
       </v-main>
     </v-container>
+    <v-spacer />
+    <footer>
+      <v-footer height="30" class="justify-end">2024 @Jannik Raskob</v-footer>
+    </footer>
   </v-app>
 </template>
 
 <script lang="ts" setup>
-  const loggedIn = ref(false)
-  const role = ref("")
+const loggedIn = ref(false)
+const role = ref("")
 
-  onMounted(() => setLogin())
-  onUpdated(() => setLogin())
+onMounted(() => setLogin())
+onUpdated(() => setLogin())
 
-  function setLogin() {
-    loggedIn.value = localStorage.getItem('jwt') != null
-    let r = localStorage.getItem("role")
-    if (r != null) {
-      role.value = r
-    }
+function setLogin() {
+  loggedIn.value = localStorage.getItem('jwt') != null
+  let r = localStorage.getItem("role")
+  if (r != null) {
+    role.value = r
   }
+}
 
-  function logout() {
-    localStorage.removeItem('jwt')
-    loggedIn.value = false
-    navigateTo('/login')
-  }
+function logout() {
+  localStorage.removeItem('jwt')
+  loggedIn.value = false
+  navigateTo('/login')
+}
 </script>
 <style>
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
+a {
+  text-decoration: none;
+  color: inherit;
+}
 </style>
